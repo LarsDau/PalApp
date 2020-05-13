3572463f5f7977b65ce00e2328dc9daca9c9d0a9
Ra Bin <agiri88@yahoo.com>
2020-05-13 18:11:31 +0200

    siehe comment

--- app/src/main/java/com/example/palapp/MainActivity.java
+++ app/src/main/java/com/example/palapp/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Lars war hier
          // so muss unser Projekt aussehen
+        // Rabin hat es angesehen
     }
 }
