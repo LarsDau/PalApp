5ae2cac0565338e2f5f03caf33eaa5bd7d262d4e
Lars Daumann <ldaumann@gmx.de>
2020-05-24 18:15:11 +0200

    User Story 3: Einloggen mit remember-Funktion

--- app/src/main/java/com/example/palapp/LoginActivity.java
+++ app/src/main/java/com/example/palapp/LoginActivity.java
@@ -2,51 +2,33 @@ package com.example.palapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.VoiceInteractor;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.JsonReader;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.HashMap;
-import java.util.Map;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class LoginActivity extends AppCompatActivity {
     Button loginBtn, registerBtn;
     EditText userET, passwordET;
+    CheckBox remBox;
 
     String validate = "http://palaver.se.paluno.uni-due.de/api/user/validate";
-    String next = "0";
     Boolean success = false;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,19 +39,76 @@ public class LoginActivity extends AppCompatActivity {
         registerBtn = findViewById(R.id.buttonRegister);
         userET = findViewById(R.id.loginUser);
         passwordET = findViewById(R.id.loginPassword);
+        remBox = findViewById(R.id.rememberBox);
+
+        preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+
+        if(preferences.getString("remember", "").equals("true")){
+            String usernameData = preferences.getString("usernameData", "");
+            String passwordData = preferences.getString("passwordData", "");
+
+            userET.setText(usernameData);
+            passwordET.setText(passwordData);
+
+            if(usernameData.length() >= 5 && passwordData.length() >= 5){
+                HashMap<String, String> params = new HashMap<>();
+                params.put("Username", usernameData);
+                params.put("Password", passwordData);
+                doLoginRequest(params);
+            }else{
+                Toast toast = Toast.makeText(getApplicationContext(), "Username und Passwort müssen mind. 5 Zeichen haben", Toast.LENGTH_LONG);
+                toast.show();
+            }
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(5000);
+                        startNextActivity();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+        } else if(preferences.getString("remember", "").equals("false")){
+            Toast.makeText(this, "Please sign in.", Toast.LENGTH_SHORT).show();
+        }
+
+        remBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(buttonView.isChecked()){
+                    SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+                    SharedPreferences.Editor editor= preferences.edit();
+                    editor.putString("remember", "true");
+                    editor.putString("usernameData", userET.getText().toString());
+                    editor.putString("passwordData", passwordET.getText().toString());
+                    editor.apply();
+                    Toast.makeText(LoginActivity.this, "Checked", Toast.LENGTH_LONG).show();
+                }else{
+                    SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+                    SharedPreferences.Editor editor= preferences.edit();
+                    editor.putString("remember", "false");
+                    editor.apply();
+                    Toast.makeText(LoginActivity.this, "Unchecked", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
-    public void goToRegisterClicked(View view){
-        Intent intent = new Intent(this , RegisterActivity.class);
-        startActivity(intent);
+    public void startNextActivity(){
+        if(success){
+            Intent intent = new Intent(this , TestLoginActivity.class);
+            startActivity(intent);
+        }
     }
 
     public void loginClicked(View view){
-        //TODO
-        //zum Testen
-
         String user = userET.getText().toString();
         String password = passwordET.getText().toString();
+
         if(user.length() >= 5 && password.length() >= 5){
             HashMap<String, String> params = new HashMap<>();
             params.put("Username", user);
@@ -79,15 +118,14 @@ public class LoginActivity extends AppCompatActivity {
             Toast toast = Toast.makeText(getApplicationContext(), "Username und Passwort müssen mind. 5 Zeichen haben", Toast.LENGTH_LONG);
             toast.show();
         }
-
-        if(success){
-            Intent intent = new Intent(this , TestLoginActivity.class);
-            startActivity(intent);
-        }
+        startNextActivity();
     }
 
     public void doLoginRequest(HashMap<String, String> params){
         RequestQueue queue = Volley.newRequestQueue(this);
+        System.out.println("Username of params" + params.get("Username"));
+        System.out.println("Password of params" + params.get("Password"));
+
         JsonObjectRequest postRequest = new JsonObjectRequest(validate,
                 new JSONObject(params),
                 new Response.Listener<JSONObject>() {
@@ -96,13 +134,13 @@ public class LoginActivity extends AppCompatActivity {
                         Toast toast = Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_LONG);
                         toast.show();
 
-                        String msg = "";
-
                         try {
                             if(response.getString("MsgType").equals("1")){
                                 success = true;
+                                System.out.println("Response Code: " + response.getString("MsgType"));
                             }else{
                                 success = false;
+                                System.out.println("Response Code: " + response.getString("MsgType"));
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -119,5 +157,10 @@ public class LoginActivity extends AppCompatActivity {
                 }
         );
         queue.add(postRequest);
+    }
+
+    public void goToRegisterClicked(View view){
+        Intent intent = new Intent(this , RegisterActivity.class);
+        startActivity(intent);
     }
 }
