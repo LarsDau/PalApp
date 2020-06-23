1d08a48a483619cfde14ad80a0a175453ae0b70e
Lars Daumann <ldaumann@gmx.de>
2020-06-23 15:13:08 +0200

    Landscape-Designs

--- app/src/main/java/com/example/palapp/LoginActivity.java
+++ app/src/main/java/com/example/palapp/LoginActivity.java
@@ -35,7 +35,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
+        setContentView(R.layout.activity_login);
 
         loginBtn = findViewById(R.id.buttonLogin);
         registerBtn = findViewById(R.id.buttonAdd);
